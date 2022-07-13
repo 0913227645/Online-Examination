3e651fbe8baf1e7f57a37e66dce0f96b58151b1b
Trần Huy <tdhuy141vt@gmail.com>
2022-07-13 23:45:41 +0700

    Initial commit

--- dash.php
+++ dash.php
@@ -1,42 +1,43 @@
 <!DOCTYPE>
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<title>DASHBOARD </title>
-<link  rel="stylesheet" href="css/bootstrap.min.css"/>
- <link  rel="stylesheet" href="css/bootstrap-theme.min.css"/>    
- <link rel="stylesheet" href="css/main.css">
- <link  rel="stylesheet" href="css/font.css">
- <script src="js/jquery.js" type="text/javascript"></script>
+    <title>DASHBOARD </title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css" />
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/font.css">
+    <script src="js/jquery.js" type="text/javascript"></script>
 
-  <script src="js/bootstrap.min.js"  type="text/javascript"></script>
- 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
 
-<script>
-$(function () {
-    $(document).on( 'scroll', function(){
-        console.log('scroll top : ' + $(window).scrollTop());
-        if($(window).scrollTop()>=$(".logo").height())
-        {
-             $(".navbar").addClass("navbar-fixed-top");
-        }
+    <script>
+    $(function() {
+        $(document).on('scroll', function() {
+            console.log('scroll top : ' + $(window).scrollTop());
+            if ($(window).scrollTop() >= $(".logo").height()) {
+                $(".navbar").addClass("navbar-fixed-top");
+            }
 
-        if($(window).scrollTop()<$(".logo").height())
-        {
-             $(".navbar").removeClass("navbar-fixed-top");
-        }
+            if ($(window).scrollTop() < $(".logo").height()) {
+                $(".navbar").removeClass("navbar-fixed-top");
+            }
+        });
     });
-});</script>
+    </script>
 </head>
 
-<body  style="background:#eee;">
-<div class="header">
-<div class="row">
-<div class="col-lg-6">
-<span class="logo">Test Your Skill</span></div>
-<?php
+<body style="background:#eee;">
+    <div class="header">
+        <div class="row">
+            <div class="col-lg-6">
+                <span class="logo">Test Your Skill</span>
+            </div>
+            <?php
  include_once 'dbConnection.php';
 session_start();
 $email=$_SESSION['email'];
@@ -52,49 +53,55 @@ include_once 'dbConnection.php';
 echo '<span class="pull-right top title1" ><span class="log1"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;&nbsp;&nbsp;&nbsp;Hello,</span> <a href="account.php" class="log log1">'.$name.'</a>&nbsp;|&nbsp;<a href="logout.php?q=account.php" class="log"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>&nbsp;Signout</button></a></span>';
 }?>
 
-</div></div>
-<!-- admin start-->
-
-<!--navigation menu-->
-<nav class="navbar navbar-default title1">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="dash.php?q=0"><b>Admin Dashboard</b></a>
+        </div>
     </div>
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li <?php if(@$_GET['q']==0) echo'class="active"'; ?>><a href="dash.php?q=0">Home </a></li>
-        <li <?php if(@$_GET['q']==1) echo'class="active"'; ?>><a href="dash.php?q=1">User</a></li>
-		<li <?php if(@$_GET['q']==2) echo'class="active"'; ?>><a href="dash.php?q=2">Ranking</a></li>
-		<li <?php if(@$_GET['q']==3) echo'class="active"'; ?>><a href="dash.php?q=3">Feedback</a></li>
-        <li class="dropdown <?php if(@$_GET['q']==4 || @$_GET['q']==5) echo'active"'; ?>">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Quiz<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="dash.php?q=4">Add Quiz</a></li>
-            <li><a href="dash.php?q=5">Remove Quiz</a></li>
-			
-          </ul>
-        </li><li class="pull-right"> <a href="logout.php?q=account.php"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>&nbsp;&nbsp;&nbsp;&nbsp;Signout</a></li>
-		
-      </ul>
-          </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-<!--navigation menu closed-->
-<div class="container"><!--container start-->
-<div class="row">
-<div class="col-md-12">
-<!--home start-->
+    <!-- admin start-->
 
-<?php if(@$_GET['q']==0) {
+    <!--navigation menu-->
+    <nav class="navbar navbar-default title1">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="dash.php?q=0"><b>Admin Dashboard</b></a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li <?php if(@$_GET['q']==0) echo'class="active"'; ?>><a href="dash.php?q=0">Home </a></li>
+                    <li <?php if(@$_GET['q']==1) echo'class="active"'; ?>><a href="dash.php?q=1">User</a></li>
+                    <li <?php if(@$_GET['q']==2) echo'class="active"'; ?>><a href="dash.php?q=2">Ranking</a></li>
+                    <li <?php if(@$_GET['q']==3) echo'class="active"'; ?>><a href="dash.php?q=3">Feedback</a></li>
+                    <li class="dropdown <?php if(@$_GET['q']==4 || @$_GET['q']==5) echo'active"'; ?>">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                            aria-expanded="false">Quiz<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="dash.php?q=4">Add Quiz</a></li>
+                            <li><a href="dash.php?q=5">Remove Quiz</a></li>
+
+                        </ul>
+                    </li>
+                    <li class="pull-right"> <a href="logout.php?q=account.php"><span class="glyphicon glyphicon-log-out"
+                                aria-hidden="true"></span>&nbsp;&nbsp;&nbsp;&nbsp;Signout</a></li>
+
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+    <!--navigation menu closed-->
+    <div class="container">
+        <!--container start-->
+        <div class="row">
+            <div class="col-md-12">
+                <!--home start-->
+
+                <?php if(@$_GET['q']==0) {
 
 $result = mysqli_query($con,"SELECT * FROM quiz ORDER BY date DESC") or die('Error');
 echo  '<div class="panel"><div class="table-responsive"><table class="table table-striped title1">
@@ -150,9 +157,9 @@ echo '</table></div></div>';}
 ?>
 
 
-<!--home closed-->
-<!--users start-->
-<?php if(@$_GET['q']==1) {
+                <!--home closed-->
+                <!--users start-->
+                <?php if(@$_GET['q']==1) {
 
 $result = mysqli_query($con,"SELECT * FROM user") or die('Error');
 echo  '<div class="panel"><div class="table-responsive"><table class="table table-striped title1">
@@ -172,10 +179,10 @@ $c=0;
 echo '</table></div></div>';
 
 }?>
-<!--user end-->
+                <!--user end-->
 
-<!--feedback start-->
-<?php if(@$_GET['q']==3) {
+                <!--feedback start-->
+                <?php if(@$_GET['q']==3) {
 $result = mysqli_query($con,"SELECT * FROM `feedback` ORDER BY `feedback`.`date` DESC") or die('Error');
 echo  '<div class="panel"><div class="table-responsive"><table class="table table-striped title1">
 <tr><td><b>S.N.</b></td><td><b>Subject</b></td><td><b>Email</b></td><td><b>Date</b></td><td><b>Time</b></td><td><b>By</b></td><td></td><td></td></tr>';
@@ -198,10 +205,10 @@ while($row = mysqli_fetch_array($result)) {
 echo '</table></div></div>';
 }
 ?>
-<!--feedback closed-->
+                <!--feedback closed-->
 
-<!--feedback reading portion start-->
-<?php if(@$_GET['fid']) {
+                <!--feedback reading portion start-->
+                <?php if(@$_GET['fid']) {
 echo '<br />';
 $id=@$_GET['fid'];
 $result = mysqli_query($con,"SELECT * FROM feedback WHERE id='$id' ") or die('Error');
@@ -217,10 +224,10 @@ echo '<div class="panel"<a title="Back to Archive" href="update.php?q1=2"><b><sp
  echo '<div class="mCustomScrollbar" data-mcs-theme="dark" style="margin-left:10px;margin-right:10px; max-height:450px; line-height:35px;padding:5px;"><span style="line-height:35px;padding:5px;">-&nbsp;<b>DATE:</b>&nbsp;'.$date.'</span>
 <span style="line-height:35px;padding:5px;">&nbsp;<b>Time:</b>&nbsp;'.$time.'</span><span style="line-height:35px;padding:5px;">&nbsp;<b>By:</b>&nbsp;'.$name.'</span><br />'.$feedback.'</div></div>';}
 }?>
-<!--Feedback reading portion closed-->
+                <!--Feedback reading portion closed-->
 
-<!--add quiz start-->
-<?php
+                <!--add quiz start-->
+                <?php
 if(@$_GET['q']==4 && !(@$_GET['step']) ) {
 echo ' 
 <div class="row">
@@ -309,10 +316,10 @@ echo '
 
 }
 ?>
-<!--add quiz end-->
+                <!--add quiz end-->
 
-<!--add quiz step2 start-->
-<?php
+                <!--add quiz step2 start-->
+                <?php
 if(@$_GET['q']==4 && (@$_GET['step'])==2 ) {
 echo ' 
 <div class="row">
@@ -385,10 +392,11 @@ echo '<div class="form-group">
 
 
 }
-?><!--add quiz step 2 end-->
+?>
+                <!--add quiz step 2 end-->
 
-<!--remove quiz-->
-<?php if(@$_GET['q']==5) {
+                <!--remove quiz-->
+                <?php if(@$_GET['q']==5) {
 
 $result = mysqli_query($con,"SELECT * FROM quiz ORDER BY date DESC") or die('Error');
 echo  '<div class="panel"><div class="table-responsive"><table class="table table-striped title1">
@@ -410,7 +418,10 @@ echo '</table></div></div>';
 ?>
 
 
-</div><!--container closed-->
-</div></div>
+            </div>
+            <!--container closed-->
+        </div>
+    </div>
 </body>
+
 </html>
